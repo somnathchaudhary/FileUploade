2f0d04d908e1d80692f43046455874a832dd069c
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-06 16:20:28 +0530

    Somnath file

---
+++ FileUplode/Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUplode.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FileUplode.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController (ILogger<UploadController>logger,IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UploadFile(Uploade fileUplode)
+        {
+            string strpath = System.IO.Path.GetExtension(fileUplode.ProfileImage.FileName);
+            if (fileUplode.ProfileImage != null && (strpath == ".jpg" && strpath == ".jpeg" && strpath == ".gif" && strpath == ".png"))
+            {
+
+                string FilePath = $"{_env.WebRootPath}/images/{fileUplode.ProfileImage.FileName}";
+                var stream = System.IO.File.Create(FilePath);
+                fileUplode.ProfileImage.CopyTo(stream);
+                return Redirect("/FileUplode/Success");
+            }
+            return Redirect("/");
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FileUplode/Models/Uploade.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUplode.Models
+{
+    public class Uploade
+    {
+        public IFormFile ProfileImage { get; set; }
+    }
+}
